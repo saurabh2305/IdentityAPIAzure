98f8e0ac325d8fee2e2de4314f4e80779a7f624f
unknown <r1m1nrao@PUNB-TR04-16.hexa.training.local>
2019-11-08 11:50:18 +0530

    initial commit

---
+++ CatalogAPI/Controllers/CatalogController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using CatalogAPI.Infrastructure;
+using CatalogAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using MongoDB.Driver;
+
+namespace CatalogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private CatalogContext db;
+
+        public CatalogController(CatalogContext db)
+        {
+            this.db = db;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("", Name ="GetProducts")]
+        public async Task<ActionResult<List<CatalogItem>>> GetProducts()
+        {
+            var result= await this.db.Catalog.FindAsync<CatalogItem>(FilterDefinition<CatalogItem>.Empty);
+            return result.ToList();
+        }
+
+        [AllowAnonymous]
+        [HttpGet("{id}", Name = "FindById")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult<CatalogItem>> FindProductById(string id)
+        {
+            var builder = Builders<CatalogItem>.Filter;
+            var filter = builder.Eq("Id", id);
+            var result = await db.Catalog.FindAsync(filter);
+            var item = result.FirstOrDefault();
+            if (item == null)
+            {
+                return NotFound(); //Not found , Status code 404
+            }
+            else
+            {
+                return Ok(item); //Found , status code 200
+            }
+        }
+
+        [Authorize(Roles ="admin")]
+        [HttpPost("", Name ="AddProduct")]
+        [ProducesResponseType((int)HttpStatusCode.Created)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public ActionResult<CatalogItem> AddProduct(CatalogItem item)
+        {
+            TryValidateModel(item);
+            if (ModelState.IsValid)
+            {
+                this.db.Catalog.InsertOne(item);
+                return Created("", item); // status code 201
+            }
+            else
+            {
+                return BadRequest(ModelState); //status code 400
+            }
+        }
+
+        [Authorize(Roles ="admin")]
+        [HttpPost("product")]
+        public ActionResult<CatalogItem> AddProduct()
+        {
+            var imageName = SaveImageToLocal(Request.Form.Files[0]);
+            var catalogItem = new CatalogItem()
+            {
+                Name=Request.Form["name"],
+                Price =Double.Parse( Request.Form["price"]),
+                Quantity = Int32.Parse(Request.Form["quantity"]),
+                ReorderLevel = Int32.Parse(Request.Form["reorderLevel"]),
+                ManufacturingDate = DateTime.Parse(Request.Form["manufacturingDate"]),
+                Vendors = new List<Vendor>(),
+                ImageUrl=imageName
+            };
+            db.Catalog.InsertOne(catalogItem);
+            return catalogItem;          
+        }
+
+        [NonAction]
+        private string SaveImageToLocal(IFormFile image)
+        {
+            var imageName = $"{Guid.NewGuid()}_{image.FileName}";
+            var dirName = Path.Combine(Directory.GetCurrentDirectory(), "Images");
+            if (!Directory.Exists(dirName))
+            {
+                Directory.CreateDirectory(dirName);
+            }
+            var filePath = Path.Combine(dirName, imageName);
+            using (FileStream fs = new FileStream(filePath, FileMode.Create))
+            {
+                image.CopyTo(fs);
+            }
+            return $"/Images/{imageName}";
+        }
+    }
+}
---
+++ CatalogAPI/Infrastructure/CatalogContext.cs
@@ -0,0 +1,36 @@
+﻿using CatalogAPI.Models;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogAPI.Infrastructure
+{
+    public class CatalogContext
+    {
+        private IConfiguration configuration;
+        private IMongoDatabase database;
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            var connectionString = configuration.GetValue<string>("MongoSettings:ConnectionString");
+            MongoClientSettings settings = MongoClientSettings.FromConnectionString(connectionString);
+            MongoClient client = new MongoClient(settings);
+            if (client != null)
+            {
+                this.database = client.GetDatabase(configuration.GetValue<string>("MongoSettings:Database"));
+            }
+        }
+
+        public IMongoCollection<CatalogItem> Catalog
+        {
+            get 
+            { 
+                return this.database.GetCollection<CatalogItem>("products"); 
+            }
+        }
+    }
+}
